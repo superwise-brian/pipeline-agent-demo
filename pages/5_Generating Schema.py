f5bdfa4208a998c6ed8d3b4ff72643d998ca5c84
Brian Mcclannahan <brian.mcclannahan@askboss.ai>
2025-05-15 16:31:32 -0500

    Added headers to generating schema page.

--- pages/5_Generating Schema.py
+++ pages/5_Generating Schema.py
@@ -8,10 +8,12 @@ project,dashboard = st.tabs(["Project Pipeline","Dashboard Pipeline"])
 with project:
     left,right = st.columns(2)
     with left:
+        st.header("Python SDK")
         with open("pipelines/project_pipeline_schema.txt", 'r') as f:
             code = f.read()
         st.code(code)
     with right:
+        st.header("JSON Schema")
         with open("pipelines/project_pipeline_schema.json", 'r') as f:
             schema = json.loads(f.read())
         st.write(schema)
@@ -20,11 +22,13 @@ with dashboard:
     
     left,right = st.columns(2)
     with left:
+        st.header("Python SDK")
         with open("pipelines/dashboard_pipeline_schema.txt", 'r') as f:
             code = f.read()
         st.code(code)
     
     with right:
+        st.header("JSON Schema")
         with open("pipelines/dashboard_pipeline_schema.json", 'r') as f:
             schema = json.loads(f.read())
         st.write(schema)
